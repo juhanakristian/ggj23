332a81607d504a0a3b9600c8163ee53a64573035
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2023-02-05 11:37:27 +0200

    fixes to movement

--- Scenes/Game/Character/Movement2.gd
+++ Scenes/Game/Character/Movement2.gd
@@ -12,31 +12,32 @@ export var speed = 20;
 var paused = false;
 onready var player = $Player
 onready var animation = $Player/AnimationPlayer
+onready var original_y = $Player.translation.y
 
 func _move_to_lane(lane):
-	var move = player.translation
+	var move = player.translation.x
 	if lane == 0:
-		move.x = $Lane0.translation.x
+		move = $Lane0.translation.x
 	elif lane == 1:
-		move.x = $Lane1.translation.x
+		move = $Lane1.translation.x
 	elif lane == 2:
-		move.x = $Lane2.translation.x
+		move = $Lane2.translation.x
 	elif lane == 3:
-		move.x = $Lane3.translation.x
+		move = $Lane3.translation.x
 	var tween = get_tree().create_tween()
-	tween.tween_property(player, "translation", move, 0.5)
+	tween.tween_property(player, "translation:x", move, 0.5)
 
 func _jump():
 	var tween = get_tree().create_tween()
 	var translation = player.translation
-	tween.tween_property(player, "translation", translation + Vector3(0, jump_height, 0), jump_duration / 2).set_trans(Tween.TRANS_EXPO)
-	tween.tween_property(player, "translation", translation, jump_duration / 2).set_trans(Tween.TRANS_QUART)
+	tween.tween_property(player, "translation:y", translation.y + jump_height, jump_duration / 2).set_trans(Tween.TRANS_EXPO)
+	tween.tween_property(player, "translation:y", original_y, jump_duration / 2).set_trans(Tween.TRANS_QUART)
 
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var is_moving = player.translation.x != $Lane1.translation.x && player.translation.x != $Lane2.translation.x && player.translation.x != $Lane0.translation.x
-	var is_jumping = player.translation.y != 0
+	var is_jumping = player.translation.y != original_y
 
 	if Input.is_action_just_pressed("move_right"):
 		lane = min(lane+1,3)
@@ -45,11 +46,13 @@ func _process(delta):
 		lane = max(lane-1,0)
 		_move_to_lane(lane)
 
-	if Input.is_action_just_pressed("jump"):
+	if !is_jumping && Input.is_action_just_pressed("jump"):
 		_jump()
 
 	
 	rotation_degrees.x = rotation_degrees.x - delta * speed
+	speed += 0.2 * delta
+	print(speed)
 	
 
 func _on_game_over():
